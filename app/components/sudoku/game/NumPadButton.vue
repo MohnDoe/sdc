1e69633f32365e2b90961289c41ae1d36b93ae4a
mohndoe <bonjour@doe.cool>
2026-02-19 15:23:48 +0100

    feat: numpad indicators

--- app/components/sudoku/game/NumPadButton.vue
+++ app/components/sudoku/game/NumPadButton.vue
@@ -10,18 +10,18 @@ const done = computed(() => remainingCount.value(num) <= 0);
 </script>
 
 <template>
-  <UButton class="relative flex flex-col gap-0 cursor-pointer bg-white elevated--xs" variant="elevated" color="neutral"
-    size="xs" @click="done ? null : insertNumber(num)">
-    <UIcon v-if="done" name="i-lucide-check" class="stroke-green-900" />
-    <span v-if="!done" class="text-xl mt-2">{{ num }}</span>
+  <UButton class="relative flex flex-col cursor-pointer bg-white items-center  elevated--xs" variant="elevated"
+    color="neutral" size="xs" @click="done ? null : insertNumber(num)">
+    <div class="h-10 flex items-center">
+      <UIcon v-if="done" name="i-lucide-check" class="stroke-green-900" />
+      <span v-if="!done" class="text-xl">{{ num }}</span>
+    </div>
     <div v-if="!done" class="
       absolute
-      top-1
-      flex flex-row gap-0.5 flex-wrap justify-center
-      w-full">
-      <div v-for="i in remainingCount(num)" class="h-1 w-1 bg-primary rounded"
+      left-1 top-1
+      grid grid-cols-3 grid-rows-3 gap-0.5 flex-wrap">
+      <div v-for="i in remainingCount(num)" class="h-1.5 w-1.5 bg-secondary rounded"
         :key="`remaining-indicator-${num - 1}-${i}`" />
     </div>
   </UButton>
-
 </template>
